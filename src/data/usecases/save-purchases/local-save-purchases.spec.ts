fd98921deb49cb502060b545778f4a838fa300df
Fabio Dias <fabio.santos@rarolabs.com.br>
2022-05-21 18:20:09 -0300

    simplify code with test help

--- src/data/usecases/save-purchases/local-save-purchases.spec.ts
+++ src/data/usecases/save-purchases/local-save-purchases.spec.ts
@@ -19,6 +19,12 @@ class CacheStoreSpy implements CacheStore {
     this.insertKey = key;
     this.insertValues = values;
   }
+
+  simulateDeleteError(): void {
+    jest.spyOn(CacheStoreSpy.prototype, "delete").mockImplementationOnce(() => {
+      throw new Error();
+    });
+  }
 }
 
 const mackPurchases = (): Array<SavePurchases.Params> => {
@@ -65,9 +71,7 @@ describe("LocalSavePurchases", () => {
 
   test("Should not insert new Cache if delete fails", () => {
     const { cacheStore, sut } = makeSut();
-    jest.spyOn(cacheStore, "delete").mockImplementationOnce(() => {
-      throw new Error();
-    });
+    cacheStore.simulateDeleteError();
     const promise = sut.save(mackPurchases());
     expect(cacheStore.insertCallsCount).toBe(0);
     expect(promise).rejects.toThrow();
